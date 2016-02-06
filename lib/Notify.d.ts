2b4fdd1245afa69c91533753eec94ace6dcfe9b3
FlippieCoetser <flippie.coetser@gmail.com>
2016-02-06 02:46:59 +0100

    Update naming convension

--- lib/Notify.d.ts
+++ lib/Notify.d.ts
@@ -1,19 +1,19 @@
-export interface notify {
+export interface notification {
     add(listener: () => void): void;
     remove(listener: () => void): void;
     trigger(...a: any[]): void;
 }
-export declare class Notify implements notify {
+export declare class Notification implements notification {
     protected _listeners: any[];
     add(listener: () => void): void;
     remove(listener?: () => void): void;
     trigger(...a: any[]): void;
 }
-export declare class MessageNotification extends Notify implements messageNotification {
+export declare class MessageNotification extends Notification implements messageNotification {
     constructor(listener?: (message) => void);
     add(listener: (message) => void): void;
 }
-export interface messageNotification extends notify {
+export interface messageNotification extends notification {
     add(listener: (message: string) => void): void;
     remove(listener: (message: string) => void): void;
     trigger(message: string): void;

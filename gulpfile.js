43d092d5918fe2e10faa95da45077a4b75145755
FlippieCoetser <flippie.coetser@gmail.com>
2016-02-06 01:42:13 +0100

    update

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+'use strict'
+var gulp = require('gulp'),
+   tsc = require('gulp-typescript'),
+   merge = require('merge2');
+   
+var tsProject = tsc.createProject('tsconfig.json');
+
+gulp.task('Build', function(){
+   var tsResult = tsProject.src()
+       .pipe(tsc(tsProject));
+   
+   return merge([
+      tsResult.dts.pipe(gulp.dest('lib')),
+      tsResult.js.pipe(gulp.dest('lib'))     
+   ]);
+   
+});
+  

967551dc26d6dc2b8a655017ec34b4433a24b721
FlippieCoetser <flippie.coetser@gmail.com>
2016-03-04 21:21:39 +0100

    add Sinon Assertion

--- test/NotifyTest.ts
+++ test/NotifyTest.ts
@@ -1,46 +1,40 @@
 /// <reference path="../typings/mocha/mocha.d.ts" />
 /// <reference path="../typings/chai/chai.d.ts" />
+/// <reference path="../typings/node/node.d.ts" />
+/// <reference path="../typings/sinon-chai/sinon-chai.d.ts" />
+
 import chai = require('chai');
+import sinon = require('sinon');
+import sinonChai = require('sinon-chai');
 let should = chai.should();
+chai.use(sinonChai);
 
 import * as Notify from '../lib/Notify';
 
 describe('Notification', () => {
     var Output: Notify.notification = new Notify.Notification();
-    var Callback: Notify.callback;
-
-            
-    it('Should Invoke Callback when .emit', (done) => {
-        Callback = (Message) => {
-            if(!Message){
-                console.log('Callback Invoked')
-                done();
-            }
-        }
-        Output.on('Test1', Callback);
-        Output.emit('Test1');
+             
+    it('Should Invoke Callback when .emit', () => {       
+        var callback = sinon.spy();
+        var event:string = 'name';
+        Output.on(event, callback);
+        Output.emit(event);
+        callback.should.have.been.called;
     });
     
-    it('Should Invoke Callback with Message when .emit', (done) => {
-       Callback = (Message) => {
-            if(Message){
-                console.log('Callback Invoked with Parameter: ' + Message);
-                done();
-            }
-        }
-       Output.on('Test2', Callback);
-       Output.emit('Test2','Message'); 
+    it('Should Invoke Callback with Message when .emit', () => {
+       var callback = sinon.spy();
+       var event:string = 'name';
+       Output.on(event, callback);
+       Output.emit(event,'Message');
+       callback.should.have.been.calledWith('Message'); 
     })
     
-    it('Should not Invoke Callback when .emit and listerner name not match', (done) => {
-       Callback = (Message) => {
-            if(!Message){
-                throw new Error('Callback Invoked');
-            }
-        }
-       Output.on('Test3', Callback);
-       Output.emit('Test4');
-       console.log('Callback NOT Invoked');
-       done()
+    it('Should not Invoke Callback when .emit and listerner name not match', () => {
+       var callback = sinon.spy();
+       var event:string = 'name';
+       Output.on(event, callback);
+       Output.emit(event + '2');
+       callback.should.not.have.been.called;
     })
 })

46d685142c4c9c173b3678eb24e06ea7acc03fde
FlippieCoetser <flippie.coetser@gmail.com>
2016-03-04 19:38:28 +0100

    add source maps

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,19 @@
 var gulp = require('gulp'),
    tsc = require('gulp-typescript'),
    merge = require('merge2'),
-   mocha = require('gulp-mocha');
+   mocha = require('gulp-mocha'),
+   sourcemaps = require('gulp-sourcemaps');
    
 var tsProject = tsc.createProject('tsconfig.json');
 
 gulp.task('Build', function(){
    var tsResult = tsProject.src()
+       .pipe(sourcemaps.init())
        .pipe(tsc(tsProject));
    
    return merge([
       tsResult.dts.pipe(gulp.dest('lib')),
-      tsResult.js.pipe(gulp.dest('lib'))     
+      tsResult.js.pipe(sourcemaps.write('./')).pipe(gulp.dest('lib'))     
    ]);
    
 });

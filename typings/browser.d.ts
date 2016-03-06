292d697f30cd9bff0b318c289f4af753aabc858e
FlippieCoetser <flippie.coetser@gmail.com>
2016-03-06 14:03:36 +0100

    npm config settings

--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -1,2 +1,5 @@
+/// <reference path="browser\ambient\chai\chai.d.ts" />
 /// <reference path="browser\ambient\mocha\mocha.d.ts" />
 /// <reference path="browser\ambient\node\node.d.ts" />
+/// <reference path="browser\ambient\sinon-chai\sinon-chai.d.ts" />
+/// <reference path="browser\ambient\sinon\sinon.d.ts" />

4cdeee8b59696dd9bdab2d7fc1673a224369b58c
FlippieCoetser <flippie.coetser@gmail.com>
2016-03-06 14:56:46 +0100

    remove node modules and typings from git

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,27 @@ var gulp = require('gulp'),
    tsc = require('gulp-typescript'),
    merge = require('merge2'),
    mocha = require('gulp-mocha'),
-   sourcemaps = require('gulp-sourcemaps');
+   sourcemaps = require('gulp-sourcemaps'),
+   clean = require('gulp-clean');
    
 var tsProject = tsc.createProject('tsconfig.json');
-
-gulp.task('build', function(){
+var config = {
+    out: 'dist'
+}
+gulp.task('build', ['clean'], function(){
    var tsResult = tsProject.src()
        .pipe(sourcemaps.init())
        .pipe(tsc(tsProject));
    
    return merge([
-      tsResult.dts.pipe(gulp.dest('lib')),
-      tsResult.js.pipe(sourcemaps.write('./')).pipe(gulp.dest('lib'))     
+      tsResult.dts.pipe(gulp.dest(config.out)),
+      tsResult.js.pipe(sourcemaps.write('./')).pipe(gulp.dest(config.out))     
    ]);
    
 });
+
+gulp.task('clean', function () {
+	return gulp.src(config.out, {read: false})
+		.pipe(clean());
+});
+
--- typings/browser.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="browser\ambient\chai\chai.d.ts" />
-/// <reference path="browser\ambient\mocha\mocha.d.ts" />
-/// <reference path="browser\ambient\node\node.d.ts" />
-/// <reference path="browser\ambient\sinon-chai\sinon-chai.d.ts" />
-/// <reference path="browser\ambient\sinon\sinon.d.ts" />

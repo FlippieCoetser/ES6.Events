9756fd492cd7271085dd8d4340cb7b0d7e04ec46
FlippieCoetser <flippie.coetser@gmail.com>
2016-03-06 19:47:58 +0100

    fix

--- test/NodeTest.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path="../typings/main.d.ts" />
-import Greeter = require('../src/Node');
-
-describe('Sayings Greeter', () => {
-    it('should greet', () => {
-        var greeter = new Greeter('John');
-        console.log(greeter.greet());
-    });
-});
